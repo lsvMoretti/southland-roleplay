ef77513482a2c1ea62ffb4adafbc864415f0e5a2
Moretti <me@unsociable.me>
2021-01-19 00:26:02 +0000

    Updated Spikes

--- Server/Groups/Police/PoliceCommands.cs
+++ Server/Groups/Police/PoliceCommands.cs
@@ -57,7 +57,7 @@ namespace Server.Groups.Police
 
                 Vector3 playerRot = new Vector3(rotation.Pitch, rotation.Roll, rotation.Yaw);
 
-                forwardPos.Z -= 1f;
+                forwardPos.Z -= 1.1f;
 
                 Prop prop = PropStreamer.Create("xs_prop_arena_spikes_02a", forwardPos, playerRot, player.Dimension, frozen: true);
 
