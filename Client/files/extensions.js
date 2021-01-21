f89e039bbcd4c3b63da05dded63ea5c2e8d82473
Moretti <me@unsociable.me>
2021-01-21 20:35:32 +0000

    .gitignore is now working

--- Client/files/extensions.js
+++
@@ -1,3 +0,0 @@
-export function Distance(positionOne, positionTwo) {
-    return Math.sqrt(Math.pow(positionOne.x - positionTwo.x, 2) + Math.pow(positionOne.y - positionTwo.y, 2) + Math.pow(positionOne.z - positionTwo.z, 2));
-}
--- Client/files/taxiHandler.js
+++
@@ -1,9 +0,0 @@
-import * as alt from 'alt-client';
-import * as native from 'natives';
-alt.onServer('setTaxiLight', setTaxiLight);
-function setTaxiLight(status) {
-    if (alt.Player.local.vehicle === undefined)
-        return;
-    var vehId = alt.Player.local.vehicle.scriptID;
-    native.setTaxiLights(vehId, status);
-}
